02f307096cc0c88291a4ad16ad9ba94ae57e780e
twenty two <51849001+twentytwo777@users.noreply.github.com>
2021-08-22 13:06:21 +0400

    new project

---
+++ owl-anti-cheat/assets/js/main.js
@@ -0,0 +1,35 @@
+const d = document;
+
+/* massive buttons */
+var navbar__toggle = d.querySelector('.navbar__toggle');
+var navbar_ml = d.querySelector('.navbar_ml');
+var logo___ml = d.querySelector('.logo___ml');
+var secondlogo = d.querySelector('.logo_hide_pc');
+var button_hide_pc = d.querySelector('.button_hide_pc');
+
+navbar__toggle.addEventListener('click', function(){
+    navbar_ml.classList.add('active');
+    secondlogo.classList.add('activef');
+    button_hide_pc.classList.add('activef');
+    d.body.style.cssText = 'overflow: hidden;';
+});
+
+button_hide_pc.addEventListener('click', function(){
+    navbar_ml.classList.remove('active');
+    navbar__toggle.removeAttribute('style');
+    d.body.removeAttribute('style');
+});
+
+//below button 
+
+var main__btn = d.querySelector('.main__btn');
+
+main__btn.addEventListener('click', function(){
+    window.scrollTo(0, 900);
+});
+
+var DateFunc = new Date();
+var copyright__info = d.querySelector('.copyright__info');
+var text_data = 'Copyright © 2021-' + DateFunc.getFullYear() + ' by OWL. All rights reserved.';
+
+copyright__info.innerHTML = text_data;
